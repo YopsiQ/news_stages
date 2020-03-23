8fb555ad88fc702f09aa1acdc4f3b5c217eeab56
YopsiQ <yopsiq@gmail.com>
2020-03-24 01:25:17 +0300

    Added stage5 with search field.

---
+++ stage5/hypernews/news/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('adding_page/', views.adding_page, name='adding_page'),
+    path('create/', views.create, name='create'),
+    path('search/', views.search, name='search'),
+    path('<str:news_link>/', views.detail, name='detail'),
+]
